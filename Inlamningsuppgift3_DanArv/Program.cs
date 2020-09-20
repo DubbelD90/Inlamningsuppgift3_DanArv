efa91947e6481122838fd6d73ac4f3947c45ed6b
DubbelD90 <dclock90@gmail.com>
2020-09-20 14:30:42 +0200

    Finished with random temp/humidity

---
+++ Inlamningsuppgift3_DanArv/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibrary.Models;
+using SharedLibrary.Services;
+using System;
+
+namespace Inlamningsuppgift3_DanArv
+{
+    class Program
+    {
+        private static readonly string _conn = "HostName=ec-win20-danarv.azure-devices.net;DeviceId=consoleApp;SharedAccessKey=TBEL/rmoCXi04vvrsMhHzgcjuZe4cx2PZGes3BaHEdo=";
+        
+
+
+        private static readonly DeviceClient deviceClient = 
+            DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+        static void Main(string[] args)
+        {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SharedLibrary/Services/DeviceService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MAD = Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibrary.Models;
+
+namespace SharedLibrary.Services
+{
+    public static class DeviceService
+    {
+
+        public static readonly Random rnd = new Random();
+        
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(10, 30),
+                    Humidity = rnd.Next(30, 60)
+                };
+
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent: {json}");
+                await Task.Delay(10 * 1000);
+            }
+        }
+
+        public static async Task SendMessageToDeviceAsync(MAD.ServiceClient serviceClient, string targetDeviceId, string message)
+        {
+            var payload = new MAD.Message(Encoding.UTF8.GetBytes(message));
+            await serviceClient.SendAsync(targetDeviceId, payload);
+        }
+    }
+}
